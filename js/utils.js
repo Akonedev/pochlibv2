f4c67097823ac448170aa8b819ed8ae23c1b67d6
Akonedev <akonedev@gmail.com>
2023-05-27 01:17:01 +0200

    add search section

--- js/utils.js
+++ js/utils.js
@@ -2,12 +2,31 @@ function addBookSection()
 {
     const bSection = document.createElement("div");
      bSection.innerHTML = `  
-        <div id="addBookSection">
+        <div id="sAddBook">
             <button id="addBook" class="btn">Ajouter un livre</button>
         </div>`;
         const newBook = document.querySelector(".newBook"); 
         newBook.after(bSection);
+ 
 
 }
+function addSearchSection()
+{
+    const cancel = addCancelButton();
+    const sSection = `<div id="sSearch">
+        <form id="form">
+        <label>Titre du livre</label><br>
+        <input type="text" name="title" id="title" class="input" required><br>
+        <label>Auteur</label><br>
+        <input type="text" name="author" id="author" class="author" required><br>
+        <button class="btn">Rechercher</button>
+        </form>       
+        </div>`;
+ 
+        const newSeach = document.querySelector(".newBook"); 
+        newSeach.after(sSection);
+
+}
+
 
 export {addBookSection};
