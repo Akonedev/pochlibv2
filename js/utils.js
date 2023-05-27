04c3a35eb8e5f8fee55c79579db111fd48ce4d4e
Akonedev <akonedev@gmail.com>
2023-05-27 02:01:32 +0200

    add on click function for add new book 2

--- js/utils.js
+++ js/utils.js
@@ -1,30 +1,41 @@
 import { addBook } from "./book.js";
 
+const newBook = document.querySelector(".newBook");
+const newSeach = document.querySelector(".newBook");
+
 function addBookSection() {
-    const bSection = document.createElement("div");
-    bSection.innerHTML = `  
-        <div id="sAddBook">
-            <button id="addBook" class="btn">Ajouter un livre</button>
-        </div>`;
-    const newBook = document.querySelector(".newBook");
+     const bSection = document.createElement("div");
+     bSection.className = 'sAddBook';
+     bSection.innerHTML = `<button id="addBook" class="btn">Ajouter un livre</button>`;
     newBook.after(bSection);
     addBook();
 
 
 }
+
+function addCancelButton()
+{
+    return `<button id="cancel" class="btn">Annuler</button>`;
+}
+
+
 function addSearchSection() {
-    const sSection = document.createElement("div");
-    sSection.innerHTML = `<div id="sSearch">
+    const cancel = addCancelButton();
+     const sSection = document.createElement("div");
+     sSection.className = 'sSearch';
+     sSection.id = "sSearch";
+    newSeach.innerHTML = `
         <form id="form">
         <label>Titre du livre</label><br>
         <input type="text" name="title" id="title" class="input" required><br>
         <label>Auteur</label><br>
         <input type="text" name="author" id="author" class="author" required><br>
         <button class="btn">Rechercher</button>
-        </form>       
-        </div>`;
-    const newSeach = document.querySelector(".newBook");
-    newSeach.after(sSection);
+        </form> 
+        ${cancel}      
+       `;
+       document.getElementById('addBook').remove();
+        newSeach.after(sSection);
 
 }
 
