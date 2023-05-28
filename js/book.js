994acccc87232ad16fdd843259cd5ff5fbfa3714
Akonedev <akonedev@gmail.com>
2023-05-28 02:54:02 +0200

    add on click function for search btn 3

--- js/book.js
+++ js/book.js
@@ -21,4 +21,46 @@ function getImage(image) {
     return image.smallThumbnail;
 }
 
-export { addBook, getDescription, getImage };
+function addBookInMyList(book) {
+    
+    const books = JSON.parse(sessionStorage.getItem('myPochList'));
+
+    if (!books) {
+        sessionStorage.setItem('myPochList', JSON.stringify([]));
+      } else {
+        const found = books.find(e => e.id == book.id);  
+        if (found) {
+            alert('ce livre existe déjà dans votre pochlist');
+            return;
+        }else{
+            books.push(book);
+            sessionStorage.setItem('myPochList', JSON.stringify(books));
+            alert("Le livre est ajouté dans votre pochlist");
+        }
+      }
+    
+   
+
+
+    // const addBookmrk =  document.getElementById('addToBookmark');
+    // addBookmrk.addEventListener('click', function () {
+    //     // const id = this.attr('id');
+    //     let value = 0;
+    //     document.querySelector('my-book').each(function () {
+    //         if(id === this.attr('id')) {
+    //             alert('Ce livre a déjà été ajouté à votre liste');
+    //             value = 1;
+    //             return false;
+    //         }
+    //     });
+    //     if(value == 0) {
+    //         const parent = $(this).closest('.book').html();
+    //         document.getElementById('content').append(`<div class="my-book" id="${id}">${parent}</div>`);
+    //         replaceBookMark();
+    //         removeBookInMyList();
+    //         addInSessionStorage(id, parent);
+    //     }
+    // })
+}
+
+export { addBook, getDescription, getImage , addBookInMyList};
--- js/results.js
+++ js/results.js
@@ -1,4 +1,4 @@
-import { getDescription, getImage } from "./book.js";
+import { getDescription, getImage, addBookInMyList } from "./book.js";
 
 function showResults(data) {
 
@@ -12,27 +12,51 @@ function showResults(data) {
 
     if (data.totalItems > 0) {
         const search = data.items;
-        search.forEach(el => {
-            const displayWrapper = document.createElement('div');
-            displayWrapper.setAttribute("id", "results");
-            displayWrapper.setAttribute("class", "book");
-            displayWrapper.innerHTML = `
-            <div class="book-top">
-            <div class="book-top-left">
-            <h3>Titre: </h3>
+        search.map(el => {
+            const bookWrapper = document.createElement('div');
+            bookWrapper.setAttribute("id", "results");
+            bookWrapper.setAttribute("class", "book");
+
+            const bookTop = document.createElement('div');
+            bookTop.setAttribute("class", "book-top");
+
+            const bookTopLeft = document.createElement('div');
+            bookTopLeft.setAttribute("class", "book-top-left");
+            bookTopLeft.innerHTML= `
+            <h3>Titre: ${el.volumeInfo.title}</h3>
             <p class="id">Id: ${el.id}</p>
-            <p>Auteur: ${el.volumeInfo.authors[0]}</p>
-            </div>
-            <div class="book-top-right">
-            <i class="fas fa-bookmark"></i>
-            </div>
-            </div>
-            <div class="book-bottom">
-            <p>Description: ${getDescription(el.volumeInfo.description)}</p>
-            <p class="center"><img src="${getImage(el.volumeInfo.imageLinks)}" alt="${el.volumeInfo.title}"></p>
-            </div>`;
-            document.getElementById("resultsBooks").append(displayWrapper);
+            <p>Auteur: ${el.volumeInfo.authors[0]}</p>`
+
+            const bookTopRight = document.createElement('div');
+            bookTopRight.setAttribute("class", "book-top-right");
+
+            const fabkmark = document.createElement('i');
+            fabkmark.setAttribute("class", "fa fa-bookmark");
+            fabkmark.setAttribute("id", "addToBookmark");
+
+            bookTopRight.appendChild(fabkmark);
+
+            const hrSeparator = document.createElement('hr');
+
+            const bookBottom = document.createElement('div');
+            bookBottom.setAttribute("class", "book-bottom");
+            bookBottom.innerHTML = `<p>Description: ${getDescription(el.volumeInfo.description)}</p>
+            <p class="center"><img src="${getImage(el.volumeInfo.imageLinks)}" alt="${el.volumeInfo.title}"></p>`
+
+            bookWrapper.appendChild(bookTop);
+            bookTop.appendChild(bookTopLeft);
+            bookTop.appendChild(bookTopRight);
+            bookWrapper.appendChild(hrSeparator);
+            bookWrapper.appendChild(bookBottom);
+
+            document.getElementById("resultsBooks").append(bookWrapper);
+           
+            //Bookmark
+            fabkmark.onclick = function () {
+            addBookInMyList(el);
+          }
         });
+        // addBookInMyList();
     }
     else {
         document.getElementById("resultsBooks").append('<p class="center">Aucun livre trouvé</p>');
